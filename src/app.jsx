580ca6863ae36add601cb411bbc3e042e9f3593b
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2021-01-28 12:37:51 +0900

    project set up & add firebase

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './app.module.css';
+import Login from './components/login/login';
+
+
+const App = () => {
+  return (
+    <>
+      <Login/>
+    </>
+  );
+};
+
+export default App;
